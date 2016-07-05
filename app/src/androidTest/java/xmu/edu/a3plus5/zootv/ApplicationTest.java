7e0d8d3e4f1deab15da68c6eeedecbd4bf5d0187
heydong <xiaochen1995226@163.com>
2016-07-06 00:29:07 +0800

    empty project

---
+++ app/src/androidTest/java/xmu/edu/a3plus5/zootv/ApplicationTest.java
@@ -0,0 +1,13 @@
+package xmu.edu.a3plus5.zootv;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
